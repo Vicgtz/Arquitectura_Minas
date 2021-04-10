94d8a70d8d4fc7b01f0d15b6bd1e3d35b845a833
TheR2D2cx <63327126+TheR2D2cx@users.noreply.github.com>
2021-04-10 12:02:16 -0800

    inicio

---
+++ MineraSoftware/src/Test/MainTests.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+/**
+ *
+ * @author R2
+ */
+public class MainTests {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
