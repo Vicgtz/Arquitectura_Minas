af5854a1940df4e4b9a01d0e65d654db5d2afd49
Bludegan <bludegan@hotmail.com>
2021-04-12 16:06:46 -0700

    Se agrego el diseño del login

--- MineraSoftware/src/Frames/FrmLogin.java
+++ MineraSoftware/src/Frames/FrmLogin.java
@@ -30,33 +30,81 @@ public class FrmLogin extends FrmBase {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         btnIniciarSesion = new javax.swing.JButton();
+        Icon_Minero = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jSeparator2 = new javax.swing.JSeparator();
+        Txt_Usuario = new javax.swing.JTextField();
+        Txt_Contraseña = new javax.swing.JPasswordField();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Login");
+        setMinimumSize(new java.awt.Dimension(868, 671));
+        setPreferredSize(new java.awt.Dimension(788, 455));
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setMinimumSize(new java.awt.Dimension(774, 584));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnIniciarSesion.setForeground(new java.awt.Color(0, 0, 255));
         btnIniciarSesion.setText("Iniciar sesión");
+        btnIniciarSesion.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 255), 1, true));
+        btnIniciarSesion.setContentAreaFilled(false);
         btnIniciarSesion.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnIniciarSesionActionPerformed(evt);
             }
         });
+        jPanel1.add(btnIniciarSesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 580, 90, 20));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(551, 551, 551)
-                .addComponent(btnIniciarSesion)
-                .addContainerGap(623, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(484, Short.MAX_VALUE)
-                .addComponent(btnIniciarSesion)
-                .addGap(204, 204, 204))
-        );
+        Icon_Minero.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Icons_Minero.png"))); // NOI18N
+        jPanel1.add(Icon_Minero, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, 420, 230));
+
+        jLabel1.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel1.setText("iniciar sesion");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 20, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 0, 255));
+        jLabel3.setText("Usuario");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 380, 70, 40));
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 0, 255));
+        jLabel4.setText("Contraseña");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 480, -1, -1));
+
+        jSeparator1.setForeground(new java.awt.Color(0, 0, 255));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 530, 240, 30));
+
+        jSeparator2.setForeground(new java.awt.Color(0, 0, 255));
+        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 440, 240, 30));
+
+        Txt_Usuario.setBorder(null);
+        jPanel1.add(Txt_Usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 420, 240, -1));
+
+        Txt_Contraseña.setBorder(null);
+        jPanel1.add(Txt_Contraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 510, 240, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 0, 460, 670));
+
+        jPanel2.setBackground(new java.awt.Color(0, 0, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Blanco con Naranja Simple Alta Gastronomía Comida Logo (1).png"))); // NOI18N
+        jLabel2.setText(" ");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(-40, -10, -1, -1));
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 410, 670));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -104,6 +152,17 @@ public class FrmLogin extends FrmBase {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Icon_Minero;
+    private javax.swing.JPasswordField Txt_Contraseña;
+    private javax.swing.JTextField Txt_Usuario;
     private javax.swing.JButton btnIniciarSesion;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
     // End of variables declaration//GEN-END:variables
 }
